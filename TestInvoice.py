1c2a612433d737216dcba2e30187b1747fdbceca
Linh <thung2@uncc.edu>
2022-04-07 21:53:53 -0400

    Initial commit

---
+++ TestInvoice.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.fixture()
+def products():
+    product = {'Peb': {'qnt': 10, 'unit_price': 3.75, 'discount': 5},
+               'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
+    return products
+
