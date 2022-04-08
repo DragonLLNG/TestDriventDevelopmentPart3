32e76a297d432a29ff02a067238d60a244b68739
Linh <thung2@uncc.edu>
2022-04-07 23:27:37 -0400

    Initial Commit

--- Invoice.py
+++ Invoice.py
@@ -6,6 +6,6 @@ class Invoice:
     def totalImprurePrice(self, products):
         total_impure_price = 0
         for k, v in products.items():
-            total_impure_price += float(v['unit_price'])*int(v['qnt'])
+            total_impure_price += float(v['unit_price']) * int(v['qnt'])
         total_impure_price = round(total_impure_price, 2)
         return total_impure_price
